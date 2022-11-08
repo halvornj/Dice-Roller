3ab84cf385799caaca4556ac78d1aa6ec496886d
halvornj <halvornjaatad@gmail.com>
2022-11-08 15:56:33 +0100

    test-update

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +11,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button2)
-        rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView2)
-            resultTextView.text = rollDice()
+        rollButton.setOnClickListener { rollDice() }
+
+        //roll on startup
+        rollDice()
+    }
+    private fun rollDice() {
+        val dice =  Dice(6)
+        val rollNum = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawResource = when(rollNum) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        diceImage.setImageResource(drawResource)
+        diceImage.contentDescription = rollNum.toString()
+
     }
 }
 class Dice(val numSides: Int) {
@@ -23,8 +39,3 @@ class Dice(val numSides: Int) {
     }
 }
 
-private fun rollDice(): String {
-    val dice =  Dice(6)
-    val rollNum: Int = dice.roll()
-    return rollNum.toString()
-}
